1e45d936687269ba674fc45cd0b3f5bbcd27f423
ar78ilnik <ar78ilnik@gmail.com>
2023-04-27 17:05:38 +0300

    menu

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -1,3 +1,15 @@
 export const func = () => {
     console.log('1');
+    const header = document.querySelector('.header');
+    const buttonMenu = header.querySelector('.button_menu');
+    const listHeader = header.querySelector('.list_header');
+    const navHeader = header.querySelector('.nav_header');
+
+    buttonMenu.addEventListener('click', () => {
+        let expanded = buttonMenu.getAttribute('aria-expanded') === 'true' || false;
+        buttonMenu.setAttribute('aria-expanded', !expanded);
+        buttonMenu.classList.toggle('button_menu--open');
+        navHeader.classList.toggle('nav_header--open');
+        listHeader.classList.toggle('list_header--open');
+    })
 }
